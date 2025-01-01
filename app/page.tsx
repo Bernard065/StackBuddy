6b75d653dc2dc4951faec704f18a28b4c05ddab1
Bernard065 <bebenibernard@gmail.com>
2025-01-01 19:41:15 +0300

    Creating the Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1 className="h1-bold font-inter">Welcome to he world of Next.js</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to he world of Next.js
-      </h1>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default Home;
