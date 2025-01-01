a623c25e6d3b143fd8eddee113efecd89e2819eb
Bernard065 <bebenibernard@gmail.com>
2025-01-01 20:04:15 +0300

    Create a theme toggler

--- components/navigation/NavBar.tsx
+++ components/navigation/NavBar.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import { ModeToggle } from "@/components/navigation/ModeToggle";
+
 const NavBar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed top-0 z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
@@ -13,7 +15,7 @@ const NavBar = () => {
       </Link>
       <p>Global Search</p>
       <div className="flex-between gap-5">
-        <p>Theme</p>
+        <ModeToggle />
         <p>Auth</p>
       </div>
     </nav>
