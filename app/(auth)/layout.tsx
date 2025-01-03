efaf465ef2f50f4eafc19c9b1e713e864cc3df29
Bernard065 <bebenibernard@gmail.com>
2025-01-03 23:31:48 +0300

    Implement React Hook Form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,15 +3,22 @@ import React from "react";
 
 import SocialAuthForm from "@/components/forms/SocialAuthForm";
 
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+interface AuthLayoutProps {
+  children: React.ReactNode;
+  variant: "signup" | "signin";
+}
+
+const AuthLayout = ({ children, variant }: AuthLayoutProps) => {
+  const headingText = variant === "signup" ? "Create Your Account" : "Sign In";
+  const paragraphText = "To continue to StackBuddy";
   return (
     <main className="flex min-h-screen items-center justify-center bg-light-auth bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-dark-auth">
       <section className="light-border background-light800_dark200 min-w-full rounded-[10px] border px-4 py-10 shadow-md sm:min-w-[520px] sm:px-8">
         <div className="flex items-center justify-between gap-2">
           <div className="space-y-2">
-            <h1 className="h2-bold text-dark100_light900">Join StackBuddy</h1>
+            <h1 className="h2-bold text-dark100_light900">{headingText}</h1>
             <p className="paragraph-regular text-dark500_light400">
-              To get your programming questions answered
+              {paragraphText}
             </p>
           </div>
           <Image
